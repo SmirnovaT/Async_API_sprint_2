bcde835e7f1591dc1434201d196a6e6fce341ab3
Kris Melikova <melikova@br-analytics.ru>
2024-05-22 22:14:15 +0300

    Removed duplicated tags

--- fastapi-solutions/src/main.py
+++ fastapi-solutions/src/main.py
@@ -42,9 +42,9 @@ app = FastAPI(
 )
 
 
-app.include_router(films.router, prefix="/api/v1/films", tags=["films"])
-app.include_router(genres.router, prefix="/api/v1/genres", tags=["genres"])
-app.include_router(persons.router, prefix="/api/v1/persons", tags=["persons"])
+app.include_router(films.router, prefix="/api/v1/films")
+app.include_router(genres.router, prefix="/api/v1/genres")
+app.include_router(persons.router, prefix="/api/v1/persons")
 
 if __name__ == "__main__":
     uvicorn.run(
