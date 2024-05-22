d3b709da5e12fc2d07f62fe64c8a3743b2103b1a
Petr Karasev <petr.karasev@datadvance.net>
2024-05-22 20:25:51 +0300

    Better waiters

--- tests/functional/utils/wait_for_service.py
+++ tests/functional/utils/wait_for_service.py
@@ -1,9 +1,21 @@
 import aiohttp
+import asyncio
 
-url = "http://service:8000/api/v1/films/"
-client_session = aiohttp.ClientSession()
-while True:
-    raw_response = await client_session.get(url)
-    status = raw_response.status
-    if status == 200:
-        break
+
+async def get_status(service_url, client):
+    raw_response = await client.get(service_url)
+    return raw_response.status
+
+
+async def wait_for_ok(service_url):
+    async with aiohttp.ClientSession() as client:
+        while True:
+            status = await get_status(service_url, client)
+            if status == 200:
+                break
+
+
+service_url = "http://service:8000/api/v1/films/"
+loop = asyncio.new_event_loop()
+loop.run_until_complete(wait_for_ok(service_url))
+
