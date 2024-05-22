c1a4c0ef456d43a1756fd306e3b194e2c60a23a6
Petr Karasev <petr.karasev@datadvance.net>
2024-05-22 19:20:51 +0300

    Small change in elastic waiter

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
         )
         if es_client.ping(error_trace=False):
             logging.info("The connection is established")
-            if es_client.count(index="movies", params={"format": "json"})["count"] == 999:
+            if es_client.count(index="movies", params={"format": "json"})["count"] > 0:
                 break
